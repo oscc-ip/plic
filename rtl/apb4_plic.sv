a3085062481cee700cdc600e3a46a07a2a80d787
maksyuki <maksyuki@126.com>
2024-02-02 20:51:11 +0800

    feat: modify if and reg def

--- rtl/apb4_plic.sv
+++ rtl/apb4_plic.sv
@@ -22,71 +22,69 @@ module apb4_plic (
   logic [3:0] s_apb4_addr;
   logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
   logic [`PLIC_PRIO_WIDTH-1:0] s_plic_prio1_d, s_plic_prio1_q;
+  logic s_plic_prio1_en;
   logic [`PLIC_PRIO_WIDTH-1:0] s_plic_prio2_d, s_plic_prio2_q;
+  logic s_plic_prio2_en;
   logic [`PLIC_PRIO_WIDTH-1:0] s_plic_prio3_d, s_plic_prio3_q;
+  logic s_plic_prio3_en;
+  logic [`PLIC_PRIO_WIDTH-1:0] s_plic_prio4_d, s_plic_prio4_q;
+  logic s_plic_prio4_en;
   logic [`PLIC_IP_WIDTH-1:0] s_plic_ip_d, s_plic_ip_q;
   logic [`PLIC_IE_WIDTH-1:0] s_plic_ie_d, s_plic_ie_q;
   logic [`PLIC_THOLD_WIDTH-1:0] s_plic_thold_d, s_plic_thold_q;
+  logic s_plic_thold_en;
   logic [`PLIC_CLAIMCOMP_WIDTH-1:0] s_plic_claimcomp_d, s_plic_claimcomp_q;
   logic [`PLIC_CLAIMCOMP_WIDTH-1:0] s_irq_max_id;
   logic [`PLIC_IRQ_NUM-1:0] s_irq_dev, s_irq_claim, s_irq_comp;
   logic [`PLIC_IRQ_NUM-1:0][`PLIC_IRQ_WID-1:0] s_irq_prio_lut_d, s_irq_prio_lut_q;
 
-  assign s_apb4_addr = apb4.paddr[5:2];
+  assign s_apb4_addr     = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
-  assign apb4.pready = 1'b1;
-  assign apb4.pslverr = 1'b0;
+  assign apb4.pready     = 1'b1;
+  assign apb4.pslverr    = 1'b0;
 
-  assign s_irq_dev = {
-    plic.crc_irq_i,
-    plic.ps2_irq_i,
-    plic.vga_irq_i,
-    plic.qspi_irq_i,
-    plic.spi_irq_i[1],
-    plic.spi_irq_i[0],
-    plic.i2s_irq_i,
-    plic.i2c_irq_i,
-    plic.tim_irq_i[3],
-    plic.tim_irq_i[2],
-    plic.tim_irq_i[1],
-    plic.tim_irq_i[0],
-    plic.wdg_irq_i,
-    plic.rtc_irq_i,
-    plic.pwm_irq_i[2],
-    plic.pwm_irq_i[1],
-    plic.pwm_irq_i[0],
-    plic.gpio_irq_i,
-    plic.uart_irq_i[1],
-    plic.uart_irq_i[0],
-    1'b0
-  };
+  assign s_irq_dev       = plic.irq_i;
+  assign plic.ext_irq_o  = s_irq_max_id > s_plic_thold_q;
 
-  assign plic.ext_irq_o = s_irq_max_id > s_plic_thold_q;
-
-  assign s_plic_prio1_d = (s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO1) ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio1_q;
-  dffr #(`PLIC_PRIO_WIDTH) u_plic_prio1_dffr (
+  assign s_plic_prio1_en = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO1;
+  assign s_plic_prio1_d  = s_plic_prio1_en ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio1_q;
+  dffer #(`PLIC_PRIO_WIDTH) u_plic_prio1_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_plic_prio1_en,
       s_plic_prio1_d,
       s_plic_prio1_q
   );
 
-  assign s_plic_prio2_d = (s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO2) ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio2_q;
-
-  dffr #(`PLIC_PRIO_WIDTH) u_plic_prio2_dffr (
+  assign s_plic_prio2_en = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO2;
+  assign s_plic_prio2_d  = s_plic_prio2_en ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio2_q;
+  dffer #(`PLIC_PRIO_WIDTH) u_plic_prio2_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_plic_prio2_en,
       s_plic_prio2_d,
       s_plic_prio2_q
   );
 
-  assign s_plic_prio3_d = (s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO3) ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio3_q;
-  dffr #(`PLIC_PRIO_WIDTH) u_plic_prio3_dffr (
+  assign s_plic_prio3_en = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO3;
+  assign s_plic_prio3_d  = s_plic_prio3_en ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio3_q;
+  dffer #(`PLIC_PRIO_WIDTH) u_plic_prio3_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_plic_prio3_en,
       s_plic_prio3_d,
       s_plic_prio3_q
+  );
+
+  assign s_plic_prio4_en = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_PRIO4;
+  assign s_plic_prio4_d  = s_plic_prio4_en ? apb4.pwdata[`PLIC_PRIO_WIDTH-1:0] : s_plic_prio4_q;
+  dffer #(`PLIC_PRIO_WIDTH) u_plic_prio4_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_plic_prio4_en,
+      s_plic_prio4_d,
+      s_plic_prio4_q
   );
 
   for (genvar i = 1; i < `PLIC_IRQ_NUM; i++) begin
@@ -98,7 +96,6 @@ module apb4_plic (
       assign s_plic_ip_d[i] = s_plic_ip_q[i];
     end
   end
-
   dffr #(`PLIC_IP_WIDTH) u_plic_ip_dffr (
       apb4.pclk,
       apb4.presetn,
@@ -113,7 +110,6 @@ module apb4_plic (
       assign s_plic_ie_d[i] = s_plic_ie_q[i];
     end
   end
-
   dffr #(`PLIC_IE_WIDTH) u_plic_ie_dffr (
       apb4.pclk,
       apb4.presetn,
@@ -121,10 +117,12 @@ module apb4_plic (
       s_plic_ie_q
   );
 
-  assign s_plic_thold_d = (s_apb4_wr_hdshk && s_apb4_addr == `PLIC_THOLD) ? apb4.pwdata[`PLIC_THOLD_WIDTH-1:0] : s_plic_thold_q;
-  dffr #(`PLIC_THOLD_WIDTH) u_plic_thold_dffr (
+  assign s_plic_thold_en = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_THOLD;
+  assign s_plic_thold_d  = s_plic_thold_en ? apb4.pwdata[`PLIC_THOLD_WIDTH-1:0] : s_plic_thold_q;
+  dffer #(`PLIC_THOLD_WIDTH) u_plic_thold_dffer (
       apb4.pclk,
       apb4.presetn,
+      s_plic_thold_en,
       s_plic_thold_d,
       s_plic_thold_q
   );
@@ -134,7 +132,6 @@ module apb4_plic (
     assign s_irq_comp[i] = s_apb4_wr_hdshk && s_apb4_addr == `PLIC_CLAIMCOMP && apb4.pwdata[`PLIC_CLAIMCOMP_WIDTH-1:0] == i;
     assign s_irq_claim[i] = s_apb4_rd_hdshk && s_apb4_addr == `PLIC_CLAIMCOMP && s_irq_max_id == i;
   end
-
   dffr #(`PLIC_CLAIMCOMP_WIDTH) u_plic_claimcomp_dffr (
       apb4.pclk,
       apb4.presetn,
@@ -146,7 +143,6 @@ module apb4_plic (
   for (genvar i = 1; i < `PLIC_IRQ_WID; i++) begin
     assign s_irq_prio_lut_d[i] = {};
   end
-
   dffr #(`PLIC_IRQ_NUM * `PLIC_IRQ_WID) u_irq_prio_lut_dffr (
       apb4.pclk,
       apb4.presetn,
@@ -161,6 +157,7 @@ module apb4_plic (
         `PLIC_PRIO1:     apb4.prdata[`PLIC_PRIO_WIDTH-1:0] = s_plic_prio1_q;
         `PLIC_PRIO2:     apb4.prdata[`PLIC_PRIO_WIDTH-1:0] = s_plic_prio2_q;
         `PLIC_PRIO3:     apb4.prdata[`PLIC_PRIO_WIDTH-1:0] = s_plic_prio3_q;
+        `PLIC_PRI43:     apb4.prdata[`PLIC_PRIO_WIDTH-1:0] = s_plic_prio4_q;
         `PLIC_IP:        apb4.prdata[`PLIC_IP_WIDTH-1:0] = s_plic_ip_q;
         `PLIC_IE:        apb4.prdata[`PLIC_IE_WIDTH-1:0] = s_plic_ie_q;
         `PLIC_THOLD:     apb4.prdata[`PLIC_THOLD_WIDTH-1:0] = s_plic_thold_q;
--- rtl/plic_define.sv
+++ rtl/plic_define.sv
@@ -12,19 +12,29 @@
 `define INC_PLIC_DEF_SV
 
 /* register mapping
+ * PLIC_CTRL:
+ * BITS:   | 31:4 | 3:1 | 0  |
+ * FIELDS: | RES  | TNM | EN |
+ * PERMS:  | NONE | RW  | RW |
+ * ------------------------------------------
  * PLIC_PRIO1:
- * BITS:   | 31:28 | ... | 7:4   | 3:0   |
- * FIELDS: | PRIO7 | ... | PRIO1 | PRIO0 |
- * PERMS:  | RW    | ... | RW    | RW    |
- * ---------------------------------------
+ * BITS:   | 31:28  | ... | 7:4    | 3:0    |
+ * FIELDS: | PRIO7  | ... | PRIO1  | PRIO0  |
+ * PERMS:  | RW     | ... | RW     | RW     |
+ * ------------------------------------------
  * PLIC_PRIO2:
- * BITS:   | 31:28  | ... | 7:4   | 3:0   |
- * FIELDS: | PRIO15 | ... | PRIO9 | PRIO8 |
- * PERMS:  | RW     | ... | RW    | RW    |
- * ----------------------------------------
+ * BITS:   | 31:28  | ... | 7:4    | 3:0    |
+ * FIELDS: | PRIO15 | ... | PRIO9  | PRIO8  |
+ * PERMS:  | RW     | ... | RW     | RW     |
+ * ------------------------------------------
  * PLIC_PRIO3:
  * BITS:   | 31:28  | ... | 7:4    | 3:0    |
  * FIELDS: | PRIO23 | ... | PRIO17 | PRIO16 |
+ * PERMS:  | RW     | ... | RW     | RW     |
+ * ------------------------------------------
+ * PLIC_PRIO3:
+ * BITS:   | 31:28  | ... | 7:4    | 3:0    |
+ * FIELDS: | PRIO31 | ... | PRIO25 | PRIO24 |
  * PERMS:  | RW     | ... | RW     | RW     |
  * ------------------------------------------
  * PLIC_IP:
@@ -51,26 +61,31 @@
 
 // prority: 0~15
 // verilog_format: off
+`define PLIC_CTRL      4'b0000 // BASEADDR + 0x00
 `define PLIC_PRIO1     4'b0001 // BASEADDR + 0x04
 `define PLIC_PRIO2     4'b0010 // BASEADDR + 0x08
 `define PLIC_PRIO3     4'b0011 // BASEADDR + 0x0C
-`define PLIC_IP        4'b0100 // BASEADDR + 0x14
-`define PLIC_IE        4'b0101 // BASEADDR + 0x18
-`define PLIC_THOLD     4'b0110 // BASEADDR + 0x1C
-`define PLIC_CLAIMCOMP 4'b1111 // BASEADDR + 0x20
+`define PLIC_PRIO4     4'b0100 // BASEADDR + 0x10
+`define PLIC_IP        4'b0101 // BASEADDR + 0x14
+`define PLIC_IE        4'b0110 // BASEADDR + 0x18
+`define PLIC_THOLD     4'b0111 // BASEADDR + 0x1C
+`define PLIC_CLAIMCOMP 4'b1000 // BASEADDR + 0x20
 
+`define PLIC_CTRL_ADDR      {26'b0, `PLIC_CTRL    , 2'b00}
 `define PLIC_PRIO1_ADDR     {26'b0, `PLIC_PRIO1    , 2'b00}
 `define PLIC_PRIO2_ADDR     {26'b0, `PLIC_PRIO2    , 2'b00}
 `define PLIC_PRIO3_ADDR     {26'b0, `PLIC_PRIO3    , 2'b00}
+`define PLIC_PRIO4_ADDR     {26'b0, `PLIC_PRIO4    , 2'b00}
 `define PLIC_IP_ADDR        {26'b0, `PLIC_IP       , 2'b00}
 `define PLIC_IE_ADDR        {26'b0, `PLIC_IE       , 2'b00}
 `define PLIC_THOLD_ADDR     {26'b0, `PLIC_THOLD    , 2'b00}
 `define PLIC_CLAIMCOMP_ADDR {26'b0, `PLIC_CLAIMCOMP, 2'b00}
 
-// not larger than 23, always plus irq0
-`define PLIC_IRQ_NUM 20 // 19 irq + irq0
+// not larger than 31+irq0
+`define PLIC_IRQ_NUM 32
 `define PLIC_IRQ_WID 5  // irq id width
 
+`define PLIC_CTRL_WIDTH      4
 `define PLIC_PRIO_WIDTH      32
 `define PLIC_IP_WIDTH        `PLIC_IRQ_NUM
 `define PLIC_IE_WIDTH        `PLIC_IRQ_NUM
@@ -79,20 +94,8 @@
 // verilog_format: on
 
 interface plic_if ();
-  logic [1:0] uart_irq_i;
-  logic       gpio_irq_i;
-  logic [2:0] pwm_irq_i;
-  logic       rtc_irq_i;
-  logic       wdg_irq_i;
-  logic [3:0] tim_irq_i;
-  logic       i2c_irq_i;
-  logic       i2s_irq_i;
-  logic [1:0] spi_irq_i;
-  logic       qspi_irq_i;
-  logic       vga_irq_i;
-  logic       ps2_irq_i;
-  logic       crc_irq_i;
-  logic       ext_irq_o;
+  logic [`PLIC_IRQ_NUM-1:0] irq_i;
+  logic                     ext_irq_o;
 endinterface
 
 `endif
