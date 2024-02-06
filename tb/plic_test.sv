671cf3b425d4bccef90cae8c99d195a928f7d2b5
maksyuki <maksyuki@126.com>
2024-02-06 10:44:56 +0800

    fix: fix no init bug for id and prio arr assignment

--- tb/plic_test.sv
+++ tb/plic_test.sv
@@ -68,7 +68,15 @@ task automatic PLICTest::test_wr_rd_reg(input bit [31:0] run_times = 1000);
 endtask
 
 task automatic PLICTest::test_irq(input bit [31:0] run_times = 10);
-  bit [`PLIC_IRQ_WIDTH-1:0] tmp_id;
+  bit [`PLIC_PRIO_WIDTH-1:0] tmp_prio1 = 32'h7654_3211;
+  bit [`PLIC_PRIO_WIDTH-1:0] tmp_prio2 = 32'hFEDC_BA98;
+  bit [ `PLIC_IRQ_WIDTH-1:0] tmp_id;
+  bit [ `PLIC_IRQ_WIDTH-1:0] tmp_id_arr                [6];
+  bit [ `PLIC_LEV_WIDTH-1:0] tmp_prio_arr              [6];
+  int                        max_prio;
+  int                        max_id;
+  int                        max_idx;
+
   super.test_irq();
   // init env
   this.write(`PLIC_CTRL_ADDR, 32'b0 & {`PLIC_CTRL_WIDTH{1'b1}});
@@ -109,6 +117,69 @@ task automatic PLICTest::test_irq(input bit [31:0] run_times = 10);
     this.write(`PLIC_CLAIMCOMP_ADDR, super.rd_data);
   end
 
+  // multi irq trg
+  for (int i = 0; i < 1000; i++) begin
+    this.plic.irq_i = '0;
+    tmp_id_arr      = {0, 0, 0, 0, 0, 0};
+    tmp_prio_arr    = {0, 0, 0, 0, 0, 0};
+    this.write(`PLIC_IE_ADDR, 32'hFFFF_FFFF & {`PLIC_IE_WIDTH{1'b1}});
+    repeat (50) @(posedge this.apb4.pclk);
+    for (int j = 0; j < 6; j++) begin
+      tmp_id = {$random} % 29 + 1;
+      if (tmp_id_arr.sum with (item == tmp_id) == 0) begin
+        tmp_id_arr[j]           = tmp_id;
+        // $display("tmp_id: %d", tmp_id);
+        this.plic.irq_i[tmp_id] = 1'b1;
+        if (tmp_id / 8 == 0 || tmp_id / 8 == 2) begin
+          // $display("perip id: %d prio: %d", tmp_id, tmp_prio1[(tmp_id%8)*4+:4]);
+          tmp_prio_arr[j] = tmp_prio1[(tmp_id%8)*4+:4];
+        end else begin
+          // $display("perip id: %d prio: %d", tmp_id, tmp_prio2[(tmp_id%8)*4+:4]);
+          tmp_prio_arr[j] = tmp_prio2[(tmp_id%8)*4+:4];
+        end
+      end
+    end
+
+    // sort the array
+    for (int j = 0; j < 5; j++) begin
+      for (int k = j; k < 6; k++) begin
+        if (k == j) begin
+          max_idx = j;
+          continue;
+        end
+        if ((tmp_prio_arr[k] > tmp_prio_arr[max_idx]) || (tmp_prio_arr[k] == tmp_prio_arr[max_idx] && tmp_id_arr[k] < tmp_id_arr[max_idx])) begin
+          max_idx = k;
+        end
+      end
+
+      // swap j, k
+      max_id                = tmp_id_arr[max_idx];
+      max_prio              = tmp_prio_arr[max_idx];
+      tmp_id_arr[max_idx]   = tmp_id_arr[j];
+      tmp_prio_arr[max_idx] = tmp_prio_arr[j];
+      tmp_id_arr[j]         = max_id;
+      tmp_prio_arr[j]       = max_prio;
+    end
+
+    // foreach (tmp_id_arr[i]) begin
+    // $display("id_arr: %d prio_arr: %d", tmp_id_arr[i], tmp_prio_arr[i]);
+    // end
+
+    for (int j = 0; j < 6; j++) begin
+      if (tmp_id_arr[j] == '0) begin
+        continue;
+      end
+      repeat (50) @(posedge this.apb4.pclk);
+      wait (this.plic.irq_o);
+      this.read(`PLIC_CLAIMCOMP_ADDR);
+      repeat (10) @(posedge this.apb4.pclk);  // sim irq handle
+      this.plic.irq_i[tmp_id_arr[j]] = 1'b0;
+      if (tmp_id_arr[j] != super.rd_data) begin
+        $display("%t [mismatch id] trg id: %d irq id: %d", $time, tmp_id_arr[j], super.rd_data);
+      end
+      this.write(`PLIC_CLAIMCOMP_ADDR, super.rd_data);
+    end
+  end
 
 endtask
 `endif
